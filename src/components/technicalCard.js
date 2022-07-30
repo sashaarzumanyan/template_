a1ec86279d17c3547990789fd16a343e1f92afb5
mkrtichminasyan <mkrtichminasyan1992@gmail.com>
2022-07-30 22:35:31 +0400

    TECHNICAL ASSISTANCE

--- src/components/technicalCard.js
+++ src/components/technicalCard.js
@@ -1,11 +1,48 @@
+import { Card, CardMedia, Typography } from '@mui/material'
 import React from 'react'
 
-const technicalCard = () => {
-  return (
-    <Box>
-        
-    </Box>
-  )
+const TechnicalCard = ({ title, img }) => {
+    return (
+
+        < Card
+            sx={{
+                // display: "flex",
+                // flexDirection: "column",
+                // justifyContent: "flex-end",
+                // alignItems: "center",
+                position: "relative",
+            }}>
+
+            <CardMedia sx={{
+                width: "100%"
+
+            }}
+                component="img"
+                alt="green iguana"
+                image={img}
+            />
+            <Typography
+                variant="h5"
+                component="div"
+                sx={{
+                    width: '100%',
+                    position: "absolute",
+                    zIndex: 1,
+                    bottom: "10%",
+                    color: "white",
+                    textAlign: "center",
+                    fontWeight: "600",
+
+
+                }}>
+                {title}
+            </Typography>
+
+        </Card>
+
+
+
+    )
 }
 
-export default technicalCard
+export default TechnicalCard
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import AboutUs from './AboutPage/AboutUs';
+import Technical_Page from './Technical Assistance/Technical_Page';
 
 
 
@@ -9,6 +10,7 @@ const Main = () => {
     <div>
         <Routes>
           <Route path='about-us' element={<AboutUs />} />
+          <Route path='technical-assistance' element={<Technical_Page />} />
         </Routes>
     </div>
   )
